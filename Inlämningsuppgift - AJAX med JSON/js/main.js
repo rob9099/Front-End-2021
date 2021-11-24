4956b35d42a9d3c614b2178e0a84840ca31e3fcc
Robert Lockett <lockettr@hotmail.com>
2021-11-24 14:59:06 +0100

    Added element creation

--- Inlämningsuppgift - AJAX med JSON/js/main.js
+++ Inlämningsuppgift - AJAX med JSON/js/main.js
@@ -8,10 +8,25 @@ async function getData (whichDataWouldYouLike) {
         console.log(data[0].name);
     
         
+        
+        let occupationString = '';
+        for(occupations of data[0].occupation){
+            occupationString += occupations + ',';
+        };
+        occupationString = occupationString.slice(0, -1);
+        
+        let result = 
+        `
+        <article>
+        <h2>Name: ${data[0].name}<h2>
+        <h3><i>Nickname: ${data[0].nickname}</i></h3>
+        <img src='${data[0].img}'>
+        <p>Occupation: ${occupationString}</p>
+        <p>Portrayed by: ${data[0].portrayed}</p>
+        <p>Status: ${data[0].status}</p>
+        `
     
-    
-    
-    
+        content.innerHTML = result;
     
     
     
@@ -23,7 +38,6 @@ async function getData (whichDataWouldYouLike) {
 }
 
 document.getElementById('generateRandomCharacterButton').addEventListener('click', function(){
-
     let randomCharacter = 'character/random';
     getData(randomCharacter);
 })
